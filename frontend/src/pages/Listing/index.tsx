462343d8c8fe210f07735e071560eb1785fb6a94
Leonam Correia <leonam236@gmail.com>
2022-01-16 21:54:50 -0300

    First Request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import MovieCard from "componentes/MovieCard";
 import Pagination from "componentes/Pagination";
+import { BASE_URL } from "utils/request";
 
 function Listing() {
+
+    //forma errada
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(Response => {
+        console.log(Response.data)
+    })
+
 
     return (
 
